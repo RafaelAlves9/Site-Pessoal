6314078f725ae450409b594b79b3a61fce58d5dc
Rafael Alves <hrafael.alves9@gmail.com>
2021-12-11 18:01:17 -0300

    up index

---
+++ script.js
@@ -0,0 +1,10 @@
+const texto = document.querySelector('.apresentation-content h2')
+
+function textoAnimado(elemento){
+    const textoArray = elemento.innerHTML.split('')
+    elemento.innerHTML = ''
+    
+    textoArray.forEach((letra, i) => {
+        setTimeout(()=> elemento.innerHTML += letra, 120 * i)})
+}
+textoAnimado(texto)
