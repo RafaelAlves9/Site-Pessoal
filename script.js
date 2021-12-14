4f1b475413bbd5244c0cee1da7d2ce9793eaf60e
Rafael Alves <hrafael.alves9@gmail.com>
2021-12-14 13:02:30 -0300

    up

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
 //mudando conteúdo do portifólio
-const queryS = (e) => document.querySelector(e)
-
-queryS('.portifolio-todos').addEventListener('click', ()=>{
-    if (queryS('.portifolio-todos').classList.contains('selectioned') == false){
+document.querySelector('.portifolio-todos').addEventListener('click', ()=>{
+    if (document.querySelector('.portifolio-todos').classList.contains('selectioned') == false){
         //adicionando class selectioned
-        queryS('.portifolio-todos').classList.add('selectioned')
-        queryS('.portifolio-javascript').classList.remove('selectioned')
-        queryS('.portifolio-bootstrap').classList.remove('selectioned')
-        queryS('.portifolio-html').classList.remove('selectioned')
+        document.querySelector('.portifolio-todos').classList.add('selectioned')
+        document.querySelector('.portifolio-javascript').classList.remove('selectioned')
+        document.querySelector('.portifolio-bootstrap').classList.remove('selectioned')
+        document.querySelector('.portifolio-html').classList.remove('selectioned')
 
-        queryS('.javascript').style.display = 'block'
-        queryS('.bootstrap').style.display = 'block'
-        queryS('.html').style.display = 'block'
+        document.querySelector('#javascript').style.display = 'block'
+        document.querySelector('#javascript1').style.display = 'block'
+        document.querySelector('#javascript2').style.display = 'block'
+        document.querySelector('#bootstrap').style.display = 'block'
+        document.querySelector('#html').style.display = 'block'
+        document.querySelector('#html1').style.display = 'block'
     }
 })
 
-queryS('.portifolio-html').addEventListener('click', ()=>{
-    if (queryS('.portifolio-html').classList.contains('selectioned') == false){
+document.querySelector('.portifolio-html').addEventListener('click', ()=>{
+    if (document.querySelector('.portifolio-html').classList.contains('selectioned') == false){
         //adicionando class selectioned
-        queryS('.portifolio-html').classList.add('selectioned')
-        queryS('.portifolio-javascript').classList.remove('selectioned')
-        queryS('.portifolio-bootstrap').classList.remove('selectioned')
-        queryS('.portifolio-todos').classList.remove('selectioned')
+        document.querySelector('.portifolio-html').classList.add('selectioned')
+        document.querySelector('.portifolio-javascript').classList.remove('selectioned')
+        document.querySelector('.portifolio-bootstrap').classList.remove('selectioned')
+        document.querySelector('.portifolio-todos').classList.remove('selectioned')
 
-        queryS('.html').style.display = 'block'
-        queryS('.javascript').style.display = 'none'
-        queryS('.bootstrap').style.display = 'none'
+        document.querySelector('#javascript').style.display = 'none'
+        document.querySelector('#javascript1').style.display = 'none'
+        document.querySelector('#javascript2').style.display = 'none'
+        document.querySelector('#bootstrap').style.display = 'none'
+        document.querySelector('#html').style.display = 'block'
+        document.querySelector('#html1').style.display = 'block'
     }
 })
 
-queryS('.portifolio-bootstrap').addEventListener('click', ()=>{
-    if (queryS('.portifolio-bootstrap').classList.contains('selectioned') == false){
+document.querySelector('.portifolio-bootstrap').addEventListener('click', ()=>{
+    if (document.querySelector('.portifolio-bootstrap').classList.contains('selectioned') == false){
         //adicionando class selectioned
-        queryS('.portifolio-bootstrap').classList.add('selectioned')
-        queryS('.portifolio-javascript').classList.remove('selectioned')
-        queryS('.portifolio-todos').classList.remove('selectioned')
-        queryS('.portifolio-html').classList.remove('selectioned')
+        document.querySelector('.portifolio-bootstrap').classList.add('selectioned')
+        document.querySelector('.portifolio-javascript').classList.remove('selectioned')
+        document.querySelector('.portifolio-html').classList.remove('selectioned')
+        document.querySelector('.portifolio-todos').classList.remove('selectioned')
 
-        queryS('.bootstrap').style.display = 'block'
-        queryS('.javascript').style.display = 'none'
-        queryS('.html').style.display = 'none'
+        document.querySelector('#javascript').style.display = 'none'
+        document.querySelector('#javascript1').style.display = 'none'
+        document.querySelector('#javascript2').style.display = 'none'
+        document.querySelector('#bootstrap').style.display = 'block'
+        document.querySelector('#html').style.display = 'none'
+        document.querySelector('#html1').style.display = 'none'
+    }
+})
+
+document.querySelector('.portifolio-javascript').addEventListener('click', ()=>{
+    if (document.querySelector('.portifolio-javascript').classList.contains('selectioned') == false){
+        //adicionando class selectioned
+        document.querySelector('.portifolio-javascript').classList.add('selectioned')
+        document.querySelector('.portifolio-bootstrap').classList.remove('selectioned')
+        document.querySelector('.portifolio-html').classList.remove('selectioned')
+        document.querySelector('.portifolio-todos').classList.remove('selectioned')
+
+        document.querySelector('#javascript').style.display = 'block'
+        document.querySelector('#javascript1').style.display = 'block'
+        document.querySelector('#javascript2').style.display = 'block'
+        document.querySelector('#bootstrap').style.display = 'none'
+        document.querySelector('#html').style.display = 'none'
+        document.querySelector('#html1').style.display = 'none'
     }
 })
