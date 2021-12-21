ef1f399b40a52bc5496dc4b4dc586be87cf89133
Rafael Alves <hrafael.alves9@gmail.com>
2021-12-21 11:33:22 -0300

    up

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const app = express()
 const bodyParser = require('body-parser')
 const db = require('./db.js')
 
-
 //configurando body-parser
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
 //criando servidor POST
-app.post('/form', async (req, res)=>{
+app.post('/form', (req, res)=>{
     res.send('Aqui estão seus dados:')
     req.body.nome
+    req.body.email
+    req.body.mensagem
 })
 app.listen(8080)
  
