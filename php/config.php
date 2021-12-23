30fc9256a469bbaac04af40fb5af7db41165f2c1
Rafael Alves <hrafael.alves9@gmail.com>
2021-12-23 18:23:24 -0300

    up

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php
 
-    $Host = 'Localhost';
-    $Username = 'root';
-    $Password = '152369Jr!';
-    $Name = 'formulario_db';
+    $Host = "localhost";
+    $Username = "root";
+    $Password = "";
+    $Name = "formulario_db";
 
     $conexao = new mysqli($Host,$Username,$Password,$Name);
 ?>
